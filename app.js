e5e7f1a551edf1e92a87cccc13040b933813b690
Jie Gao <jg3526@users.noreply.github.com>
2015-08-27 00:28:50 -0400

    added error.jade for 404

--- app.js
+++ app.js
@@ -88,10 +88,9 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
 	app.use(function(err, req, res, next) {
 		res.status(err.status || 500);
-		res.render('error', {
-			message: err.message,
-			error: err
-		});
+		
+		req.flash('error', err);
+		res.render('error');
 	});
 }
 
